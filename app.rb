c071cf031272dc1c6e25be9658fa26cc9b129525
mandareis <amanda@depaula.xyz>
2020-10-26 07:42:00 -0700

    commit tree 24258fbbf475e15cff9244e466bbf4da00fe859c

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 class App < Sinatra::Base
-
-	get '/' do
-		erb :index
-	end
-
-
+  get "/" do
+    erb :index
+  end
+  get "/hello" do
+    erb :hello
+  end
+  get "/goodbye" do
+    erb :goodbye
+  end
 end
